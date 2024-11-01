1e36830e9e887b674dcf3290f45864a55d72b142
Maxwell Severiano <maxwellseveriano91@gmail.com>
2024-11-01 14:49:00 -0300

    Usando tailwind

--- routes/web.ts
+++ routes/web.ts
@@ -1,12 +1,1 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
-
-router.on('/').render('pages/home')
