4cda55234bf07b13fdc5f26521d8686c45252d90
Maxwell Severiano <maxwellseveriano91@gmail.com>
2024-11-02 13:34:24 -0300

    brincadeirinha estilosa na rota skins

--- app/controllers/skins_controller.ts
+++ app/controllers/skins_controller.ts
@@ -2,8 +2,8 @@ import Skin from '#models/skin'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class SkinsController {
-    async index({view}:HttpContext){
-        const skins = await Skin.all()
-        return view.render('pages/skins', {skins})
-    } 
+  async index({ view }: HttpContext) {
+    const skins = await Skin.all()
+    return view.render('pages/skins', { skins })
+  }
 }
--- routes/web.ts
+++ routes/web.ts
@@ -1,13 +1,11 @@
-const CasesController = () => import('#controllers/cases_controller')
 import router from '@adonisjs/core/services/router'
-const SkinsController = () => import('#controllers/skins_controller') 
-const StickersController = () => import('#controllers/stickers_controller') 
 
-router.get('/skins', [SkinsController, 'index']
-)
+const CasesController = () => import('#controllers/cases_controller')
+const SkinsController = () => import('#controllers/skins_controller')
+const StickersController = () => import('#controllers/stickers_controller')
 
-router.get('/cases', [CasesController, 'index']
-)
+router.get('/skins', [SkinsController, 'index'])
 
-router.get('/stickers', [StickersController, 'index']
-)
+router.get('/cases', [CasesController, 'index'])
+
+router.get('/stickers', [StickersController, 'index'])
