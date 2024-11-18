1c1999a78eea7cfcbd0db11c49c2ca1086cd6340
Maxwell Severiano <maxwellseveriano91@gmail.com>
2024-11-18 02:05:00 -0300

    navbar e tela de login

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 export default {
   content: ['./resources/**/*.edge', './resources/**/*.{js,ts,jsx,tsx,vue,edge}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': '#1e293b',
+        'card-color': '#374151',
+        'h-card-color': '#4b5563',
+        'nav-color': '#0f172a',
+        'card-border-color': '#1f2937',
+        'text-color': '#f3f4f6',
+      },
+    },
   },
-  colors: {
-    'primary-color': 'blue-500',
-  },
+
   plugins: [],
 }
