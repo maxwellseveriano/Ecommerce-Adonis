22191215e893d036cdf1b4f977485b54c1ffad2d
Maxwell Severiano <maxwellseveriano91@gmail.com>
2024-12-17 21:14:49 -0300

    skins filtradas por categoria

--- app/controllers/skins_controller.ts
+++ app/controllers/skins_controller.ts
@@ -3,11 +3,15 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class SkinsController {
   async index({ view, request }: HttpContext) {
-    const priceRange = request.only(['min', 'max'])
-    if (!priceRange.min && !priceRange.max) {
-      const skins = await Skin.all()
+    const priceRange = request.only(['min', 'max', 'categorias'])
+
+    if (priceRange.categorias) {
+      const skins = await Skin.query()
+        .whereBetween('price', [priceRange.min || -1, priceRange.max || Infinity])
+        .whereIn('category', priceRange.categorias)
       return view.render('pages/skins/index', { skins })
     }
+
     const skins = await Skin.query().whereBetween('price', [
       priceRange.min || -1,
       priceRange.max || Infinity,
